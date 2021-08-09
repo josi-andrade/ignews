f1e4fb6c381630a84f1b6e716f392955e58445cd
josi-andrade <josi.c.andrade1988@gmail.com>
2021-08-09 20:52:57 -0300

    login with github

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { AppProps } from "next/app";
 import React from "react";
 import { Header } from "../components/header";
+import { Provider as NextAuthProvider } from "next-auth/client";
 import "../style/global.scss";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
